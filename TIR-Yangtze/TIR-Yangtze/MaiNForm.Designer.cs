2c6f96188d76905f244498a49bc9fb9a038e3426
pkugiser <1500012638@pku.edu.cn>
2019-11-08 16:20:25 +0800

    initialize mainform

---
+++ TIR-Yangtze/TIR-Yangtze/MaiNForm.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace TIR_Yangtze
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.文件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.打开ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.运行ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.关于ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.splitContainer = new System.Windows.Forms.SplitContainer();
+            this.splitContainerLeft = new System.Windows.Forms.SplitContainer();
+            this.lyrTreeView1 = new MapUserInterface.LyrTreeView();
+            this.line1 = new DevComponents.DotNetBar.Controls.Line();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).BeginInit();
+            this.splitContainer.Panel1.SuspendLayout();
+            this.splitContainer.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerLeft)).BeginInit();
+            this.splitContainerLeft.Panel1.SuspendLayout();
+            this.splitContainerLeft.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.文件ToolStripMenuItem,
+            this.运行ToolStripMenuItem,
+            this.关于ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1089, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 文件ToolStripMenuItem
+            // 
+            this.文件ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.打开ToolStripMenuItem});
+            this.文件ToolStripMenuItem.Name = "文件ToolStripMenuItem";
+            this.文件ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.文件ToolStripMenuItem.Text = "文件";
+            // 
+            // 打开ToolStripMenuItem
+            // 
+            this.打开ToolStripMenuItem.Name = "打开ToolStripMenuItem";
+            this.打开ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.打开ToolStripMenuItem.Text = "打开";
+            this.打开ToolStripMenuItem.Click += new System.EventHandler(this.打开ToolStripMenuItem_Click);
+            // 
+            // 运行ToolStripMenuItem
+            // 
+            this.运行ToolStripMenuItem.Name = "运行ToolStripMenuItem";
+            this.运行ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.运行ToolStripMenuItem.Text = "运行";
+            // 
+            // 关于ToolStripMenuItem
+            // 
+            this.关于ToolStripMenuItem.Name = "关于ToolStripMenuItem";
+            this.关于ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.关于ToolStripMenuItem.Text = "关于";
+            // 
+            // splitContainer
+            // 
+            this.splitContainer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.splitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer.Name = "splitContainer";
+            // 
+            // splitContainer.Panel1
+            // 
+            this.splitContainer.Panel1.Controls.Add(this.splitContainerLeft);
+            this.splitContainer.Size = new System.Drawing.Size(1064, 557);
+            this.splitContainer.SplitterDistance = 234;
+            this.splitContainer.TabIndex = 1;
+            // 
+            // splitContainerLeft
+            // 
+            this.splitContainerLeft.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.splitContainerLeft.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainerLeft.Location = new System.Drawing.Point(0, 0);
+            this.splitContainerLeft.Name = "splitContainerLeft";
+            this.splitContainerLeft.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainerLeft.Panel1
+            // 
+            this.splitContainerLeft.Panel1.Controls.Add(this.lyrTreeView1);
+            this.splitContainerLeft.Size = new System.Drawing.Size(234, 557);
+            this.splitContainerLeft.SplitterDistance = 386;
+            this.splitContainerLeft.TabIndex = 0;
+            // 
+            // lyrTreeView1
+            // 
+            this.lyrTreeView1.BackColor = System.Drawing.SystemColors.Window;
+            this.lyrTreeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lyrTreeView1.DrawMode = System.Windows.Forms.TreeViewDrawMode.OwnerDrawText;
+            this.lyrTreeView1.IsMultiSelect = false;
+            this.lyrTreeView1.Location = new System.Drawing.Point(0, 0);
+            this.lyrTreeView1.Name = "lyrTreeView1";
+            this.lyrTreeView1.Size = new System.Drawing.Size(232, 384);
+            this.lyrTreeView1.TabIndex = 0;
+            // 
+            // line1
+            // 
+            this.line1.ForeColor = System.Drawing.Color.DarkRed;
+            this.line1.Location = new System.Drawing.Point(13, 29);
+            this.line1.Name = "line1";
+            this.line1.Size = new System.Drawing.Size(1064, 23);
+            this.line1.TabIndex = 2;
+            this.line1.Text = "line1";
+            this.line1.Thickness = 6;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.splitContainer);
+            this.panel1.Location = new System.Drawing.Point(13, 58);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1064, 557);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(13, 28);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1064, 24);
+            this.panel2.TabIndex = 4;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1089, 624);
+            this.Controls.Add(this.line1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.splitContainer.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).EndInit();
+            this.splitContainer.ResumeLayout(false);
+            this.splitContainerLeft.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerLeft)).EndInit();
+            this.splitContainerLeft.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 文件ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 运行ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 关于ToolStripMenuItem;
+        private System.Windows.Forms.SplitContainer splitContainer;
+        private System.Windows.Forms.SplitContainer splitContainerLeft;
+        private MapUserInterface.LyrTreeView lyrTreeView1;
+        private DevComponents.DotNetBar.Controls.Line line1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ToolStripMenuItem 打开ToolStripMenuItem;
+    }
+}
---
+++ TIR-Yangtze/TIR-Yangtze/MaiNForm.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace TIR_Yangtze
+{
+    public partial class MainForm : Form
+    {
+
+        #region 字段
+        private string _imgPath;  // 影像文件路径
+        private string _baseName; // 影像文件名(GF5文件夹采用指定格式)
+        #endregion
+
+        #region 构造函数
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region 控件控制
+        private void 打开ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbg = new FolderBrowserDialog();
+            if (fbg.ShowDialog() == DialogResult.OK)
+            {
+                _imgPath = fbg.SelectedPath;
+                _baseName = Path.GetFileNameWithoutExtension(_imgPath);
+                
+                Debug.WriteLine(fbg.SelectedPath);
+            }
+
+        }
+        #endregion
+
+        #region 函数
+        private void OpenFile()
+        {
+            
+        }
+
+        #endregion
+    }
+}
